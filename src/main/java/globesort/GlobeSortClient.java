21efe689a599134e85fd1f7dc140a017c04bf135
Tian Qiu <qiu58@purdue.edu>
2018-05-09 13:29:36 -0700

    i

--- src/main/java/globesort/GlobeSortClient.java
+++ src/main/java/globesort/GlobeSortClient.java
@@ -50,7 +50,7 @@ public class GlobeSortClient {
         long client_request_end = System.currentTimeMillis();
 
         long sortTime = response.getSortTime();
-        double bytePerSec = values.length / ((client_request_end - client_request_start - sortTime) * 0.5) * 4;
+        double bytePerSec = values.length / ((client_request_end - client_request_start - sortTime) * 0.5) * 4000;
         System.out.print("bytePerSec = ");
         System.out.println(bytePerSec);
     }
